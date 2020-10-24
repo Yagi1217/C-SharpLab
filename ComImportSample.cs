38bfe6cf8938714af5c6feab575bb86b0524ce6e
Yagi1217 <m.yagi.busi@gmail.com>
2020-10-24 14:15:23 +0900

    Create ComImportSample.cs
    以前勤めていたところで作成した社内向けプログラミング資料に
    記載していたコードになります。
    …多分、参照元そのままの様な気もしているので、メモ程度で
    
    フレームワークは「Visual studio」を使っていました。

---
+++ ComImportSample.cs
@@ -0,0 +1,18 @@
+using MSXML2;
+using System;
+namespace NativeInterop {
+  class ComImportSample {
+    static void Main() {
+      var doc = new DOMDocument60();
+      if (doc.load("Sample.xml")) {
+        var s = doc.documentElement;
+        foreach (IXMLDOMElement item in s.getElementsByTagName("Item")) {
+          var name = item.getAttribute("Name");
+          var value = item.getAttribute("Value");
+
+          Console.WriteLine($"{name} = {value}");
+        }
+      }
+    }
+  }
+}
